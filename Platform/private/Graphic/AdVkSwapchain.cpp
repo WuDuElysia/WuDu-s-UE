b5764f8ca636fa383d95c51490657b1454bdaacc
WuDuElysia <2270942826@qq.com>
2025-07-21 19:19:10 +0800

    7211919

--- Platform/private/Graphic/AdVkSwapchain.cpp
+++ Platform/private/Graphic/AdVkSwapchain.cpp
@@ -35,6 +35,9 @@ namespace ade {
 		}
 
 		// 确定图像共享模式和队列家庭索引
+		/*这段代码用于设置 Vulkan 图像的共享模式及相关的队列家族索引，具体功能如下：
+		如果图形和呈现队列家族相同，则使用独占模式（VK_SHARING_MODE_EXCLUSIVE），无需指定队列家族索引。
+		否则使用并发模式（VK_SHARING_MODE_CONCURRENT），并指定两个不同的队列家族索引，分别用于图形和呈现操作。*/
 		VkSharingMode imageSharingMode;
 		uint32_t queueFamilyIndexCount;
 		uint32_t pQueueFamilyIndices[2] = { 0, 0 };
@@ -54,24 +57,43 @@ namespace ade {
 
 		// 填充交换链创建信息结构体
 		VkSwapchainCreateInfoKHR swapchainInfo = {
-		    swapchainInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
-		    swapchainInfo.pNext = nullptr,
-		    swapchainInfo.flags = 0,
-		    swapchainInfo.surface = mContext->GetSurface(),
-		    swapchainInfo.minImageCount = imageCount,
-		    swapchainInfo.imageFormat = mSurfaceInfo.surfaceFormat.format,
-		    swapchainInfo.imageColorSpace = mSurfaceInfo.surfaceFormat.colorSpace,
-		    swapchainInfo.imageExtent = mSurfaceInfo.capabilities.currentExtent,
-		    swapchainInfo.imageArrayLayers = 1,
-		    swapchainInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
-		    swapchainInfo.imageSharingMode = imageSharingMode,
-		    swapchainInfo.queueFamilyIndexCount = queueFamilyIndexCount,
-		    swapchainInfo.pQueueFamilyIndices = pQueueFamilyIndices,
-		    swapchainInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
-		    swapchainInfo.compositeAlpha = VK_COMPOSITE_ALPHA_INHERIT_BIT_KHR,
-		    swapchainInfo.presentMode = mSurfaceInfo.presentMode,
-		    swapchainInfo.clipped = VK_FALSE,
-		    swapchainInfo.oldSwapchain = oldSwapchain
+			// 指定结构体类型
+			swapchainInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+			// 指向下一个结构体的指针，此处不需要链接其他结构体
+			swapchainInfo.pNext = nullptr,
+			// 保留字段，用于未来使用，目前应设置为0
+			swapchainInfo.flags = 0,
+			// 分配给交换链的表面
+			swapchainInfo.surface = mContext->GetSurface(),
+			// 最小图像数量，影响渲染性能和内存使用
+			swapchainInfo.minImageCount = imageCount,
+			// 图像的格式
+			swapchainInfo.imageFormat = mSurfaceInfo.surfaceFormat.format,
+			// 图像的颜色空间
+			swapchainInfo.imageColorSpace = mSurfaceInfo.surfaceFormat.colorSpace,
+			// 图像的分辨率
+			swapchainInfo.imageExtent = mSurfaceInfo.capabilities.currentExtent,
+			// 图像的层数，通常为1，用于2D渲染
+			swapchainInfo.imageArrayLayers = 1,
+			// 图像的使用方式，此处作为颜色附件
+			swapchainInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+			// 图像共享模式，决定如何在队列之间共享图像
+			swapchainInfo.imageSharingMode = imageSharingMode,
+			// 队列族索引数量，用于支持图像共享模式
+			swapchainInfo.queueFamilyIndexCount = queueFamilyIndexCount,
+			// 队列族索引指针，指定参与图像共享的队列族
+			swapchainInfo.pQueueFamilyIndices = pQueueFamilyIndices,
+			// 表面变换方式，此处设置为无变换
+			swapchainInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
+			// 组合alpha值，此处设置为继承
+			swapchainInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+			//swapchainInfo.compositeAlpha = VK_COMPOSITE_ALPHA_INHERIT_BIT_KHR,
+			// 呈现模式，影响渲染的流畅性和延迟
+			swapchainInfo.presentMode = mSurfaceInfo.presentMode,
+			// 是否允许图像被裁剪，减少需要呈现的像素数量
+			swapchainInfo.clipped = VK_FALSE,
+			// 旧的交换链，用于交换链的重建
+			swapchainInfo.oldSwapchain = oldSwapchain
 		};
 		// 创建交换链
 		VkResult ret = vkCreateSwapchainKHR(mDevice->GetHandle(), &swapchainInfo, nullptr, &mHandle);
@@ -91,21 +113,27 @@ namespace ade {
 		return ret == VK_SUCCESS;
 	}
 
+	// 设置交换链的表面能力
 	void AdVKSwapchain::SetupSurfaceCapabilities() {
-		// capabilities
+		// 获取物理设备的表面能力
 		vkGetPhysicalDeviceSurfaceCapabilitiesKHR(mContext->GetPhyDevice(), mContext->GetSurface(), &mSurfaceInfo.capabilities);
 
+		// 获取设备的设置
 		AdVkSettings settings = mDevice->GetSettings();
 
-		// format
+		// 获取表面格式数量
 		uint32_t formatCount;
 		CALL_VK(vkGetPhysicalDeviceSurfaceFormatsKHR(mContext->GetPhyDevice(), mContext->GetSurface(), &formatCount, nullptr));
 		if (formatCount == 0) {
 			LOG_E("{0} : num of surface format is 0", __FUNCTION__);
 			return;
 		}
+
+		// 根据格式数量创建格式数组并获取格式信息
 		std::vector<VkSurfaceFormatKHR> formats(formatCount);
 		CALL_VK(vkGetPhysicalDeviceSurfaceFormatsKHR(mContext->GetPhyDevice(), mContext->GetSurface(), &formatCount, formats.data()));
+
+		// 查找设备设置中指定的表面格式
 		int32_t foundFormatIndex = -1;
 		for (int i = 0; i < formatCount; i++) {
 			if (formats[i].format == settings.surfaceFormat && formats[i].colorSpace == VK_COLORSPACE_SRGB_NONLINEAR_KHR) {
@@ -113,21 +141,27 @@ namespace ade {
 				break;
 			}
 		}
+
+		// 如果未找到指定格式，则使用第一个格式
 		if (foundFormatIndex == -1) {
 			foundFormatIndex = 0;
 		}
 		mSurfaceInfo.surfaceFormat = formats[foundFormatIndex];
 
-		// present mode
+		// 获取表面呈现模式数量
 		uint32_t presentModeCount;
 		CALL_VK(vkGetPhysicalDeviceSurfacePresentModesKHR(mContext->GetPhyDevice(), mContext->GetSurface(), &presentModeCount, nullptr));
 		if (presentModeCount == 0) {
 			LOG_E("{0} : num of surface present mode is 0", __FUNCTION__);
 			return;
 		}
+
+		// 根据呈现模式数量创建呈现模式数组并获取呈现模式信息
 		std::vector<VkPresentModeKHR> presentModes(presentModeCount);
 		CALL_VK(vkGetPhysicalDeviceSurfacePresentModesKHR(mContext->GetPhyDevice(), mContext->GetSurface(), &presentModeCount, presentModes.data()));
-		VkPresentModeKHR preferredPresentMode = mDevice->GetSettings().presentMode;
+
+		// 查找设备设置中指定的呈现模式
+		VkPresentModeKHR preferredPresentMode = settings.presentMode;
 		int32_t foundPresentModeIndex = -1;
 		for (int i = 0; i < presentModeCount; i++) {
 			if (presentModes[i] == preferredPresentMode) {
@@ -135,6 +169,8 @@ namespace ade {
 				break;
 			}
 		}
+
+		// 如果找到指定的呈现模式，则使用它；否则使用第一个呈现模式
 		if (foundPresentModeIndex >= 0) {
 			mSurfaceInfo.presentMode = presentModes[foundPresentModeIndex];
 		}
@@ -143,18 +179,34 @@ namespace ade {
 		}
 	}
 
+	/**
+	* @brief 获取交换链中的下一个可用图像
+	*
+	* 该函数通过vkAcquireNextImageKHR从交换链中获取一个可用的图像，并可选地使用信号量或围栏来同步
+	*
+	* @param outImageIndex 指向一个整数的指针，用于存储获取的图像索引
+	* @param semaphore 可选的信号量，用于在图像获取后发出信号
+	* @param fence 可选的围栏，用于在图像获取后发出信号
+	* @return VkResult 返回获取图像的结果，可能为VK_SUCCESS、VK_SUBOPTIMAL_KHR或其他错误代码
+	*/
 	VkResult AdVKSwapchain::AcquireImage(int32_t* outImageIndex, VkSemaphore semaphore, VkFence fence) {
 		uint32_t imageIndex;
+		// 调用vkAcquireNextImageKHR获取下一个可用图像的索引
 		VkResult ret = vkAcquireNextImageKHR(mDevice->GetHandle(), mHandle, UINT64_MAX, semaphore, fence, &imageIndex);
+
+		// 如果提供了围栏，则等待围栏被触发并重置围栏
 		if (fence != VK_NULL_HANDLE) {
 			CALL_VK(vkWaitForFences(mDevice->GetHandle(), 1, &fence, VK_FALSE, UINT64_MAX));
 			CALL_VK(vkResetFences(mDevice->GetHandle(), 1, &fence));
 		}
 
+		// 如果成功获取了图像或图像状态为次优，更新外部和内部的当前图像索引
 		if (ret == VK_SUCCESS || ret == VK_SUBOPTIMAL_KHR) {
 			*outImageIndex = imageIndex;
 			mCurrentImageIndex = imageIndex;
 		}
+
+		// 返回获取图像的结果
 		return ret;
 	}
 
--- Sample/SandBox/Main.cpp
+++ Sample/SandBox/Main.cpp
@@ -2,6 +2,9 @@
 #include"Adlog.h"
 #include"AdWindow.h"
 #include"AdGraphicContext.h"
+#include"Graphic/AdVKGraphicContext.h"
+#include"Graphic/AdVKDevice.h"
+#include"Graphic/AdVKSwapchain.h"
 
 int main() {
 	ade::Adlog::Init();
@@ -9,9 +12,14 @@ int main() {
 
 	std::unique_ptr<ade::AdWindow> window = ade::AdWindow::Create(800, 600, "sandbox");
 	std::unique_ptr<ade::AdGraphicContext> graphicContext = ade::AdGraphicContext::Create(window.get());
+	auto vkgraaphicContext = dynamic_cast<ade::AdVKGraphicContext*>(graphicContext.get());
+	std::unique_ptr<ade::AdVKDevice> vkdevice = std::make_unique<ade::AdVKDevice>(vkgraaphicContext, 2, 2);
+	std::unique_ptr<ade::AdVKSwapchain> vkswapchain = std::make_unique<ade::AdVKSwapchain>(vkgraaphicContext,vkdevice.get());
+
 	while (!window->ShouldClose()) {
 		window->PollEvents();
 		window->SwapBuffer();
 	}
 	return EXIT_SUCCESS;
+	
 }
